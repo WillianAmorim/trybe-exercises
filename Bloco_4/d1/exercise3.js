0f04f538063fd9ad7bfe0c63cfe220ed540e7f47
Willian Silva Amorim <willian.amorim1@outlook.com>
2021-05-21 02:30:25 -0300

    adicionado exercicio 3

---
+++ Bloco_4/d1/exercise3.js
@@ -0,0 +1,13 @@
+let n1 = 1500
+let n2 = 50 
+let n3 = 300
+
+if (n1 > n2 && n1 > n3) {
+    console.log ("O maior número é: " + n1)
+}
+else if (n2 > n1 && n2 > n3) {
+    console.log("O maior número é: " + n2)
+}
+else {
+    console.log("O maior número é: " + n3)
+}
