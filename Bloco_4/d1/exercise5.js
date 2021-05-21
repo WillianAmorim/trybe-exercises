58b5ef78277825d34bdff9b653fa839f707c3ba8
Willian Silva Amorim <willian.amorim1@outlook.com>
2021-05-21 03:11:24 -0300

    adicionado exercicio 5js

---
+++ Bloco_4/d1/exercise5.js
@@ -0,0 +1,16 @@
+let ang1 = 90
+let ang2 = 45
+let ang3 = 45
+let total = (ang1 + ang2 + ang3)
+
+if (total === 180) {
+    console.log(true)
+}
+else if (ang1 === 0 | ang2 === 0 | ang3 === 0) {
+    console.log("erro");
+}
+else {
+    console.log(false)
+}
+
+
