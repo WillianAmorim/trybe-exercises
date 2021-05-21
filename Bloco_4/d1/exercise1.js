96257b7610e4125d2a49777018d2f668a6778493
Willian Silva Amorim <willian.amorim1@outlook.com>
2021-05-21 01:57:02 -0300

    arquivos nao monitorados

---
+++ Bloco_4/d1/exercise1.js
@@ -0,0 +1,12 @@
+let n1 = 10;
+let n2 = 10;
+
+console.log("Adição = " + (n1 + n2));
+
+console.log("Subtração = " + (n1 - n2));
+
+console.log("Multiplicação = " + (n1 * n2));
+
+console.log("Divisão = " + (n1 / n2));
+
+console.log("Resto da divisão = " + (n1 % n2));
