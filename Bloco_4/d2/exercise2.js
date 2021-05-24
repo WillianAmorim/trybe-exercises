6d93c3dd21c27c6b023079e7d0220a6331b3e14e
WillianAmorim <willian.amorim1@outlook.com>
2021-05-24 11:55:07 -0300

    adicionado exercicio 1 e 2

---
+++ Bloco_4/d2/exercise2.js
@@ -0,0 +1,7 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0
+
+for(contador = 0; contador < numbers.length; contador++){
+    total += numbers[contador]
+}
+console.log(total)
