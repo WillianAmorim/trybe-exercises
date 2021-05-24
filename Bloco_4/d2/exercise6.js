daaf0aeee363abe81934d7983b212e4579ce22d8
WillianAmorim <willian.amorim1@outlook.com>
2021-05-24 14:49:24 -0300

    adicionado exercicio 6

---
+++ Bloco_4/d2/exercise6.js
@@ -0,0 +1,7 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(contador = 0; contador < numbers.length; contador ++){
+    if((numbers[contador]%2) != 0){
+    console.log(numbers[contador])
+    }
+}
