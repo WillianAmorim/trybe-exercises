77192e913d0f7d7453ab9638ecd8872fe7c43505
WillianAmorim <willian.amorim1@outlook.com>
2021-05-27 23:45:42 -0300

    adicionado exercicio 7

---
+++ Bloco_4/d2/exercicio7.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (contador = 1; contador < numbers.length; contador += 1) {
+    if (numbers[contador] < numbers[0]) {
+        numbers[0] = numbers[contador]
+        
+    }
+}
+console.log (numbers[0])
